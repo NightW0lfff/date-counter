7b337b6fd5102b7bda0dfb992ba05b2e10c5bc22
NightW0lfff <88699008+NightW0lfff@users.noreply.github.com>
2024-03-22 11:44:30 +1100

    Finalised Date Counter Project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
   return (
     <div className="main">
-      <div></div>
+      <Container />
+    </div>
+  );
+}
+
+function Container() {
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const addStep = () => {
+    setStep((s) => s + 1);
+    console.log(step);
+  };
+
+  const subStep = () => {
+    setStep((s) => (s === 0 ? s : s - 1));
+  };
+
+  const addDate = () => {
+    setCount((c) => c + 1);
+  };
+
+  const subDate = () => {
+    setCount((c) => (c === 0 ? c : c - 1));
+  };
+
+  const date = () => {
+    const curDate = new Date();
+    curDate.setDate(curDate.getDate() + step * count);
+    return curDate.toDateString();
+  };
+
+  return (
+    <>
+      <Counter content={`Step : ${step}`} add={addStep} sub={subStep} />
+      <Counter content={`Count : ${count}`} add={addDate} sub={subDate} />
+      <h3>{`${step * count} days from today is ${date()}`}</h3>
+    </>
+  );
+}
+
+function Counter({ content, add, sub }) {
+  return (
+    <div className="container">
+      <button className="btn" onClick={sub}>
+        -
+      </button>
+      <h4>{content}</h4>
+      <button className="btn" onClick={add}>
+        +
+      </button>
     </div>
   );
 }
